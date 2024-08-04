50a7d03823844816889233abd0f890cc829e8350
helfekih <hamdi.elfekih@groupe-telnet.net>
2024-08-04 15:13:56 +0100

    configure routing of signUp and signIn components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
-
+const routes: Routes = [
+  {path:"signUp", component:SignUpComponent},
+  {path: "signIn", component:SignInComponent},
+  
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule,RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
