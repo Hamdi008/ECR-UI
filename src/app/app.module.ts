47ae957c7e2bf13aea38b81305897265d5690ead
helfekih <hamdi.elfekih@groupe-telnet.net>
2024-08-04 14:20:15 +0100

    delete logIn component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { LogInComponent } from './components/log-in/log-in.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +12,6 @@ import { LogInComponent } from './components/log-in/log-in.component';
     HeaderComponent,
     FooterComponent,
     SignUpComponent,
-    LogInComponent
   ],
   imports: [
     BrowserModule
